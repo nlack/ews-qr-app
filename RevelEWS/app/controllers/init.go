15d26235b0b742b892a1faa874696d7c24eed86f
Michael Sann <rollcage@Michaels-MacBook-Pro.local>
2017-07-09 00:05:21 +0200

    first version adopted from https://rclayton.silvrback.com/revel-gorp-and-mysql

---
+++ RevelEWS/app/controllers/init.go
@@ -0,0 +1,79 @@
+package controllers
+
+import (
+	"database/sql"
+	"fmt"
+	"strings"
+
+	"github.com/coopernurse/gorp"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/nlack/ews-qr-app/RevelEWS/app/models"
+	"github.com/revel/revel"
+)
+
+func init() {
+	revel.InterceptMethod((*GorpController).Begin, revel.BEFORE)
+	revel.InterceptMethod((*GorpController).Commit, revel.AFTER)
+	revel.InterceptMethod((*GorpController).Rollback, revel.FINALLY)
+}
+
+func getParamString(param string, defaultValue string) string {
+	p, found := revel.Config.String(param)
+	if !found {
+		if defaultValue == "" {
+			revel.ERROR.Fatal("Cound not find parameter: " + param)
+		} else {
+			return defaultValue
+		}
+	}
+	return p
+}
+
+func getConnectionString() string {
+	host := getParamString("db.host", "")
+	port := getParamString("db.port", "3306")
+	user := getParamString("db.user", "")
+	pass := getParamString("db.password", "password")
+	dbname := getParamString("db.name", "auction")
+	protocol := getParamString("db.protocol", "tcp")
+	dbargs := getParamString("dbargs", " ")
+
+	if strings.Trim(dbargs, " ") != "" {
+		dbargs = "?" + dbargs
+	} else {
+		dbargs = ""
+	}
+	return fmt.Sprintf("%s:%s@%s([%s]:%s)/%s%s",
+		user, pass, protocol, host, port, dbname, dbargs)
+}
+
+var InitDb func() = func() {
+	connectionString := getConnectionString()
+	if db, err := sql.Open("mysql", connectionString); err != nil {
+		revel.ERROR.Fatal(err)
+	} else {
+		Dbm = &gorp.DbMap{
+			Db:      db,
+			Dialect: gorp.MySQLDialect{"InnoDB", "UTF8"}}
+	}
+	// Defines the table for use by GORP
+	// This is a function we will create soon.
+	defineBidItemTable(Dbm)
+	if err := Dbm.CreateTablesIfNotExists(); err != nil {
+		revel.ERROR.Fatal(err)
+	}
+}
+
+func defineBidItemTable(dbm *gorp.DbMap) {
+	// set "id" as primary key and autoincrement
+	t := dbm.AddTable(models.BidItem{}).SetKeys(true, "id")
+	// e.g. VARCHAR(25)
+	t.ColMap("name").SetMaxSize(25)
+}
+
+func init() {
+	revel.OnAppStart(InitDb)
+	revel.InterceptMethod((*GorpController).Begin, revel.BEFORE)
+	revel.InterceptMethod((*GorpController).Commit, revel.AFTER)
+	revel.InterceptMethod((*GorpController).Rollback, revel.FINALLY)
+}
